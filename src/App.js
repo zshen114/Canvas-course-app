3bc724903e0b3e6c92b418dc922ec84d1f8bb7a7
zshen114 <shen.zhixu@northeastern.edu>
2023-10-05 04:34:25 -0500

    a2 Submit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="../public/kanbas/home.html"
           target="_blank"
           rel="noopener noreferrer"
         >
